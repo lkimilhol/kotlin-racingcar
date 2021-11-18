2b8bd456511084f0f3d4f3ca0860cfbc54bfa4fe
lkimihol <lkimilhol@gmail.com>
2021-11-18 23:51:56 +0900

    test(PositiveTest): Displayname 설정

--- src/test/kotlin/PositiveTest.kt
+++ src/test/kotlin/PositiveTest.kt
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
+@DisplayName("자동차 이름 테스트")
 internal class PositiveTest {
     @Test
     fun `음수를 가지는 Positive는 생성 불가능 하다`() {
