609a2698057a395afffc046acf51ff5fb7501090
lkimihol <lkimilhol@gmail.com>
2021-11-18 23:40:49 +0900

    feat(Positive): 양수 생성

---
+++ src/main/kotlin/Positive.kt
@@ -0,0 +1,14 @@
+data class Positive private constructor(val number: Int) {
+    companion object {
+        fun create(number: Int): Positive {
+            validate(number)
+            return Positive(number)
+        }
+
+        private fun validate(number: Int) {
+            if (number < 0) {
+                throw RuntimeException("")
+            }
+        }
+    }
+}
